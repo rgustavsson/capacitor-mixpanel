2c440e8d91ce4b733da31f7531d8ef96773585fd
Richard Gustavsson <richard.gustavsson@afya.se>
2025-03-20 22:15:46 +0100

    Definitions

---
+++ dist/esm/dist/esm/index.js
@@ -0,0 +1,6 @@
+import { registerPlugin } from '@capacitor/core';
+const Mixpanel = registerPlugin('Mixpanel', {
+    web: () => import('./web').then((m) => new m.MixpanelWeb()),
+});
+export * from './definitions';
+export { Mixpanel };
